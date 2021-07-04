4c818fa2e754d9df01b63d798dad8cc9791dcee3
Yoshiteru Toyoda <6089more@gmai.com>
2021-07-04 19:27:52 +0000

    [Finish] 2—7　確認問題１

---
+++ lesson7.rb
@@ -0,0 +1,10 @@
+puts "計算をはじめます"
+input_message = gets.to_i
+input_message1 = gets.to_i
+puts "2つの値を入力してください"
+puts input_message
+puts input_message1
+puts "計算結果を出力します"
+puts "計算結果は#{input_message * input_message1}"
+
+puts "計算を終了します"
